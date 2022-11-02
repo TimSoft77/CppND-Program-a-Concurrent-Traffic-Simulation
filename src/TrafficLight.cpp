19e8b1026c2b9585407863888469a55a499c5029
Tim Aske <timaske@hotmail.com>
2022-11-01 21:50:42 -0700

    tweaks following successful submission

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -64,7 +64,7 @@ void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
     std::shared_ptr<MessageQueue<TrafficLightPhase>> _messageQueue(new MessageQueue<TrafficLightPhase>);
-    threads.emplace_back(std::thread(&cycleThroughPhases, this));
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
